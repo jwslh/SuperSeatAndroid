1a7d6824816f763b2028cd6b9d919802e4592e57
CalesQ <1264251638@qq.com>
2021-05-21 22:21:49 +0800

    add:添加图书馆时间格式转化方法

--- app/src/main/java/indi/wkq/superseatandroid/constant/MyURL.kt
+++ app/src/main/java/indi/wkq/superseatandroid/constant/MyURL.kt
@@ -14,4 +14,12 @@ object MyURL {
     const val PATH_RESERVATIONS = "/rest/v2/user/reservations"
 
     const val PATH_HISTORY = "/rest/v2/history/{page}/{pageSize}"
+
+    const val PATH_BOOK = "/rest/v2/freeBook"
+
+    const val PATH_CANCEL = "/rest/v2/cancel/{reserveId}"
+
+    const val PATH_STOP = "/rest/v2/stop"
+
+    const val PATH_SEARCH = "/rest/v2/searchSeats/{date}/{start}/{endTime}"
 }
--- app/src/main/java/indi/wkq/superseatandroid/utils/TimeUtils.kt
+++ app/src/main/java/indi/wkq/superseatandroid/utils/TimeUtils.kt
@@ -16,7 +16,7 @@ object TimeUtils {
     fun getTimeStampFromTimeText(timeText: String): Long =
         SimpleDateFormat(TIME_TEXT_PATTERN).parse(timeText).time
 
-    fun getTomorrowDate() : String {
+    fun getTomorrowDate(): String {
         val calendar = Calendar.getInstance()
         calendar.apply {
             time = Date()
@@ -26,7 +26,30 @@ object TimeUtils {
         return SimpleDateFormat(DATE_PATTERN).format(calendar.time)
     }
 
-    fun getTodayDate() : String = SimpleDateFormat(DATE_PATTERN).format(Date().time)
+    fun getTodayDate(): String = SimpleDateFormat(DATE_PATTERN).format(Date().time)
 
-    fun getNowTimeText() : String = SimpleDateFormat(TIME_PATTERN).format(Date().time)
+    fun getNowTimeText(): String = SimpleDateFormat(TIME_PATTERN).format(Date().time)
+
+    /**
+     * 时分格式 转为图书馆需要的 整数格式
+     */
+    fun getTimeFromTimeText(timeText: String): String {
+        var arrs = timeText.split(":")
+        if (arrs.size == 2) {
+            return (arrs[0].toInt() * 60 + arrs[1].toInt()).toString()
+        }
+        return ""
+    }
+
+    /**
+     *  图书馆整数时间格式 转为 日常时分格式
+     */
+    fun getTimeTextFromTime(time: String): String {
+        if (time != "" && time != "0") {
+            var h = time.toInt() / 60
+            var m = time.toInt() % 60
+            return (if (h >= 10) h.toString() else "0$h") + ":" + if (m >= 10) m.toString() else "0$m"
+        }
+        return ""
+    }
 }
